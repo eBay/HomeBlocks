6c31f7411c29a6cad9f9a0835a4444883e1fa337
Brian Szmyd <bszmyd@ebay.com>
2023-02-14 16:42:04 -0700

    Add back unit test runs.

--- conanfile.py
+++ conanfile.py
@@ -65,6 +65,7 @@ class HomeReplicationConan(ConanFile):
         cmake = CMake(self)
         cmake.configure(defs=definitions)
         cmake.build()
+        cmake.test(target=test_target, output_on_failure=True)
 
     def package(self):
         lib_dir = join(self.package_folder, "lib")
