c7856e8c13e83b4a8890f93d0a775b9af4cf7bdc
Yaming Kuang <yaming.kuang@gmail.com>
2023-02-13 17:50:11 -0700

    SDSTOR-8752 map pba structure definition

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ required_conan_version = ">=1.50.0"
 
 class HomeReplicationConan(ConanFile):
     name = "home_replication"
-    version = "0.0.3"
+    version = "0.0.4"
     homepage = "https://github.com/eBay/HomeReplication"
     description = "Fast Storage Replication using NuRaft"
     topics = ("ebay")
