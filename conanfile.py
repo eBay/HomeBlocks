5f53de66100b378056b3d8e09f457899a459be92
Yaming Kuang <yaming.kuang@gmail.com>
2025-02-21 12:27:31 -0700

    upgrade sisl ver

--- conanfile.py
+++ conanfile.py
@@ -46,7 +46,7 @@ class HomeBlocksConan(ConanFile):
     def requirements(self):
         self.requires("homestore/[^6.5]@oss/master")
         self.requires("iomgr/[^11.3]@oss/master")
-        self.requires("sisl/[~12.2, include_prerelease=True]@oss/master", transitive_headers=True)
+        self.requires("sisl/[^12.3, include_prerelease=True]@oss/master", transitive_headers=True)
         self.requires("lz4/1.9.4", override=True)
 
     def validate(self):
