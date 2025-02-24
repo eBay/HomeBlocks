8ed8b2e8de23cdd07065b1bd2eb0355b31c6691b
Brian Szmyd <bszmyd@ebay.com>
2025-02-24 11:02:58 -0700

    Patch bump.

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ required_conan_version = ">=1.60.0"
 
 class HomeBlocksConan(ConanFile):
     name = "homeblocks"
-    version = "0.0.2"
+    version = "0.0.3"
 
     homepage = "https://github.com/eBay/HomeBlocks"
     description = "Block Store built on HomeStore"
