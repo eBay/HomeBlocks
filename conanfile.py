aa8be68e7ec2606adcf156e438b488c94b2f1f2d
Yaming Kuang <yaming.kuang@gmail.com>
2025-04-21 09:30:10 -0700

    Issue 72 Volume desstroy crash recovery

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ required_conan_version = ">=1.60.0"
 
 class HomeBlocksConan(ConanFile):
     name = "homeblocks"
-    version = "0.0.7"
+    version = "0.0.8"
     
     homepage = "https://github.com/eBay/HomeBlocks"
     description = "Block Store built on HomeStore"
