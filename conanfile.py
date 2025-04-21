3f35b0263a8c479b9d1724b6de7f2d69f85c172d
Sanal P <sanebay3@gmail.com>
2025-04-21 11:32:57 -0700

    Implement homeblock write path.
    Write flow writes data to disk, add key value to index
    and flush journal entry to journal. Add test case to test
    the normal io flow.

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ required_conan_version = ">=1.60.0"
 
 class HomeBlocksConan(ConanFile):
     name = "homeblocks"
-    version = "0.0.13"
+    version = "0.0.14"
     homepage = "https://github.com/eBay/HomeBlocks"
     description = "Block Store built on HomeStore"
     topics = ("ebay")
